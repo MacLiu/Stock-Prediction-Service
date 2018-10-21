5338ecce12ca64d99759eccee48555741e7b1e88
mliu <mliu@iastate.edu>
2018-10-20 21:37:45 -0500

    Added ddashbaord

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from watson_developer_cloud.natural_language_understanding_v1 import Features, E
 
 import json
 import requests
+import psycopg2
 
 app = Flask(__name__)
 
@@ -26,11 +27,12 @@ natural_language_understanding = NaturalLanguageUnderstandingV1(
 )
 
 cache = base.Client(('localhost', 11211))
+conn = psycopg2.connect(database="hack-isu", user="postgres", password="twitterbot1", host="35.232.221.83", port="5432")
 
 @app.route('/')
 @app.route('/index.html')
 def get_dashboard():
-	return render_template('pages/index.html')
+	return render_template('index.html')
 
 @app.route('/get_stock_prediction')
 def get_stock_prediction():
@@ -38,32 +40,42 @@ def get_stock_prediction():
 
 	if not ticker_symbol:
 		return jsonify("Please make sure to send a ticker symbol")
-
-	# Fetch from database for ticker - use mock for now.
-	# TODO (macliu): Load stock tweets from database for recency.
-	prediction_score = cache.get(ticker_symbol)
+	   
+	prediction_score = None#cache.get(ticker_symbol)
+	top_tweets = []
 	if not prediction_score:
 		total_score = 0
-		count = 0
-		print("MISS")
-		global mock_stock_tweet_list
-		for stock_tweet in mock_stock_tweet_list:
-			if stock_tweet["symbol"] == ticker_symbol:
-				total_score += get_stock_analysis_score(stock_tweet["tweet"]) * stock_tweet["tweeter_score"]
-				count += 1
+		positive_count = 0
+		neutral_count = 0
+		negative_count = 0
+		cur = conn.cursor()
+		cur.execute("SELECT * from public.tweets where ticker='{}';".format(ticker_symbol))
+		stock_tweet_list = cur.fetchall()
+		print(stock_tweet_list)
+		for stock_tweet in stock_tweet_list:
+			score = get_stock_analysis_score(stock_tweet[2])
 
+			if score > 0:
+				positive_count += 1
+				if score > 1:
+					top_tweets.append(stock_tweet)
+			elif score < 0:
+				negative_count += 1;
+			else:
+				neutral_count += 1;
+
+			total_score += score * stock_tweet[3]
+
+		count = positive_count + neutral_count + negative_count
 		if count == 0:
 			return jsonify("We have no data on: {}".format(ticker_symbol))
 
 		prediction_score = total_score / count
-	else:
-		print("HIT")
 
 	# Write to cache
-	cache.set(ticker_symbol, prediction_score)
+	#cache.set(ticker_symbol, prediction_score)
 
-	return jsonify("The score for {} is: {}".format(ticker_symbol, prediction_score))
-
+	return render_template('index.html', ticker=ticker_symbol, score=prediction_score, positive_count=positive_count, negative_count=negative_count)
 
 def get_stock_analysis_score(tweet):
 	global natural_language_understanding
